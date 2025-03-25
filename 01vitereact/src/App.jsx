a9f6a55b4d2aa5972d96bfffc02f9d2cfcad3da1
imab7564 <imab7564@gmail.com>
2025-03-25 07:21:26 +0530

    Component of React

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,9 +1,21 @@
 
+
+import Chai from "./chai" 
+
 function App() {
 
   return (
+    <>
+    {/* it is called fragment */}
+    <Chai />
     <h1>chai aur react with vite | Abhishek</h1>
+    </>
   )
 }
 
 export default App
+
+// Learned About Some Best Practices Using React
+// -> Name the component capitalize e.g App.js
+// -> We can name both App.js & App.jsx but in vite development environment we have to use jsx
+// -> We can return only one component by a jsx function so it's good to wrap in <></> or <div></div>
