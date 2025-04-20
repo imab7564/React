09dbdb123dce05200dff7983be81d65f48b9a094
imab7564 <imab7564@gmail.com>
2025-04-20 18:23:25 +0530

    done with context api and local storage

---
+++ 10todoContextApi/src/contexts/index.js
@@ -0,0 +1,4 @@
+//hmlog index.js file isliye banate h taki sara files ko idhar import kar le
+//  or jisko v dena h wo idhar se hi de de
+
+export {TodoContext,TodoProvider,useTodo} from "./TodoContext"
